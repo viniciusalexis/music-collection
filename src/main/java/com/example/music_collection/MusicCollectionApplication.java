828428a37adb0b8a2b827733963e4546512821b1
Vinicius Alex <vinicius.alex@unidesk.com.br>
2024-01-06 13:38:48 -0300

    Initial commit

---
+++ src/main/java/com/example/music_collection/MusicCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCollectionApplication.class, args);
+	}
+
+}
