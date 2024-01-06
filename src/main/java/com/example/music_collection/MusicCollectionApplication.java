98d788fa0fd7c420d77591a1882a4f6eed3fbd36
Vinicius Alex <vinicius.alex@unidesk.com.br>
2024-01-06 13:42:50 -0300

    Configurações iniciais framework e bd.

--- src/main/java/com/example/music_collection/MusicCollectionApplication.java
+++ src/main/java/com/example/music_collection/MusicCollectionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicCollectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicCollectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicCollectionApplication.class, args);
+    }
 
 }
